073e8bfc23da4f09bd5f1bd6ea911fe36b783073
Suleman Khalil <105054957+suleman42244@users.noreply.github.com>
2023-09-30 21:20:53 +0500

    made a simple game rock paper scissors

--- Rock-Paper/main.js
+++ Rock-Paper/main.js
@@ -1,6 +1,37 @@
-function playerSelection(playerChoice) {
+function playerSelection() {
   playerChoice = String(prompt("Please enter your choice:")).toLowerCase();
-  console.log(playerChoice);
+  return playerChoice;
 }
 
-playerSelection();
+
+function getComputerChoice() {
+  let computerChoice = Math.round(Math.random() * 2);
+  if(computerChoice === 0){
+    return "rock"
+  } else if(computerChoice === 1){
+    return "paper"
+  } else if(computerChoice === 2){
+    return "scissors"
+  }
+}
+getComputerChoice();
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){
+      return "it's a tie";
+    } else if(
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+    ){
+      return "You win";
+    } else{
+      return "Computer win";
+    }
+}
+
+const getPlayerSelection = playerSelection();
+const computerSelection = getComputerChoice();
+console.log(`Player chosse: ${getPlayerSelection}`);
+console.log(`Computer choose: ${computerSelection}`)
+console.log(playRound(getPlayerSelection, computerSelection))
